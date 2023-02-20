cd4bfc024139300eb14ce8efc34b656b1b37ec65
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-02-20 16:35:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//STEP 1: request name, surname and color with a prompt
+//STEP 2: Generate the result that merge all the variables 
+//STEP 3: print in console log and on screen
+
+
+
+let name = prompt("Inserisci il tuo nome:");
+let surname = prompt("Inserisci il tuo cognome:");
+let color = prompt("Inserisci il tuo colore preferito:");
+let result = name + " " + surname + " " + color;
+
+
+//Print on screen
+document.write(result);
+
+//Print on console
+console.log(result)
